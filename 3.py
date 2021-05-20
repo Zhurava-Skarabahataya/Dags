005921b01107190b598fdb4f4fcb29f2d8078ff2
Anastasiya Zhurava-Skarabahataya <anastasiya_zhurava-skarabahataya@epam.com>
2021-05-21 00:12:16 +0300

    idd11ggikks3sd1py

---
+++ 3.py
@@ -0,0 +1,67 @@
+try:
+
+    from datetime import timedelta, datetime
+    from airflow import DAG
+    from airflow.operators.python_operator import PythonOperator
+    from airflow.operators.bash_operator import BashOperator
+    from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
+    from pyspark.sql import SparkSession
+    from pyspark.sql import *
+
+    print("All Dag modules are ok ......")
+except Exception as e:
+    print("Error  {} ".format(e))
+
+
+def first_function_execute(**context):
+    print("HELLO ")
+    
+
+
+def second_function_execute(**context):
+    print("Is it me you looking for")
+   
+
+
+default_args = {
+    "owner": "airflow",
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5),
+    "depends_on_past": False,
+    "start_date": datetime(2021, 1, 1),
+}
+
+
+with DAG(
+        dag_id="3",
+        schedule_interval="@once",
+        default_args=default_args,
+        catchup=False) as f:
+
+    first_f = PythonOperator(
+        task_id="first",
+        python_callable=first_function_execute,
+        provide_context=True,
+        op_kwargs={"name":"Soumil Shah"}
+    )
+    
+    second_f = PythonOperator(
+        task_id="second",
+        python_callable=second_function_execute,
+        provide_context=True,
+    )
+    
+    spark_op = SparkSubmitOperator(
+   task_id='spark_submit_job',
+    application='/home/ubuntu/test.py',
+    total_executor_cores='1',
+    executor_cores='1',
+    executor_memory='2g',
+    num_executors='1',
+    name='airflow-spark',
+    verbose=False,
+    driver_memory='1g',
+    conf={'master':'local'},
+    dag=dag)
+
+first_f >> second_f >> spark_submit_job
