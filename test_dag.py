e3ad6bd5492e63c9a9fa74f10facc624ddfa3d07
Anastasiya Zhurava-Skarabahataya <anastasiya_zhurava-skarabahataya@epam.com>
2021-05-20 23:12:59 +0300

    idd11ggikkss

---
+++ test_dag.py
@@ -0,0 +1,56 @@
+try:
+
+    from datetime import timedelta, datetime
+    from airflow import DAG
+    from airflow.operators.python_operator import PythonOperator
+    from airflow.operators.bash_operator import BashOperator
+    from airflow.contrib.operators.spark_submit_operator import SparkSubmitOperator
+    from pyspark.sql import SparkSession
+    from pyspark.sql import *
+
+    print("All Dag modules are ok ......")
+except Exception as e:
+    print("Error  {} ".format(e))
+
+
+def first_function_execute(**context):
+    print("HELLO ")
+    
+
+
+def second_function_execute(**context):
+    print("Is it me you looking for")
+   
+
+
+default_args = {
+    "owner": "airflow",
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5),
+    "depends_on_past": False,
+    "start_date": datetime(2021, 1, 1),
+}
+
+
+with DAG(
+        dag_id="test_dag",
+        schedule_interval="@once",
+        default_args=default_args,
+        catchup=False) as f:
+
+    first_f = PythonOperator(
+        task_id="first",
+        python_callable=first_function_execute,
+        provide_context=True,
+        op_kwargs={"name":"Soumil Shah"}
+    )
+    
+    second_f = PythonOperator(
+        task_id="second",
+        python_callable=second_function_execute,
+        provide_context=True,
+    )
+    
+    
+
+first_f >> second_f 
