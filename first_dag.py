1960aec07e45a6e6ce4526b6b246dc4c7c87c523
Anastasiya Zhurava-Skarabahataya <anastasiya_zhurava-skarabahataya@epam.com>
2021-05-20 16:05:55 +0300

    sec91

--- first_dag.py
+++ first_dag.py
@@ -1,10 +1,9 @@
 try:
 
-    from datetime import timedelta
+    from datetime import timedelta, datetime
     from airflow import DAG
     from airflow.operators.python_operator import PythonOperator
-    from datetime import datetime
-    import pandas as pd
+    from airflow.operators.bash_operator import BashOperator
 
     print("All Dag modules are ok ......")
 except Exception as e:
@@ -20,6 +19,8 @@ def second_function_execute(**context):
     print("Is it me you looking for")
 
 
+
+
 with DAG(
         dag_id="first_dag",
         schedule_interval="@daily",
@@ -27,6 +28,7 @@ with DAG(
             "owner": "airflow",
             "retries": 1,
             "retry_delay": timedelta(minutes=5),
+            "depends_on_past": False,
             "start_date": datetime(2021, 1, 1),
         },
         catchup=False) as f:
